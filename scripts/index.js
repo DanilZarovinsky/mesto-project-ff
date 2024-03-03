1200c25ae1a2a52ebb7d78ada77513c01f286e76
Danil Zarovinsky <danilzarovinsky@yandex.ru>
2024-03-03 19:44:42 +0500

    Готов

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(nameValue, linkValue) {
+  // @todo: Темплейт карточки
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardElement.querySelector(".card__title").textContent = nameValue;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleleCard);
+
+  cardContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
-
+function deleleCard(event) {
+  const deletingCard = event.target.closest(".card");
+  deletingCard.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+  addCard(card.name, card.link);
+});
